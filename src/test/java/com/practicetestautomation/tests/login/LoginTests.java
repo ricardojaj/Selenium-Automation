96dffc1f484461293f5b1bf7612cd3f4bd27490e
ricardojaj <rickanjos85@gmail.com>
2025-06-15 17:30:30 -0300

    Refactoring - login tests in a single class

--- src/test/java/com/practicetestautomation/tests/login/LoginTests.java
+++ src/test/java/com/practicetestautomation/tests/login/LoginTests.java
@@ -8,7 +8,50 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class NegativeLoginTests {
+public class LoginTests {
+
+    @Test
+    public void testLoginFunctionality(){
+        //Open page
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://practicetestautomation.com/practice-test-login/");
+
+        //Type username student into Username field
+        WebElement usernameField = driver.findElement(By.id("username"));
+        usernameField.sendKeys("student");
+
+        //Type password Password123 into Password field
+        WebElement passwordField = driver.findElement(By.id("password"));
+        passwordField.sendKeys("Password123");
+
+
+        //Push Submit button
+        WebElement submitBtn = driver.findElement(By.id("submit"));
+        submitBtn.click();
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        //Verify new page URL contains practicetestautomation.com/logged-in-successfully/
+        String expectedUrl = "https://practicetestautomation.com/logged-in-successfully/";
+        String actualUrl = driver.getCurrentUrl();
+        Assert.assertEquals(actualUrl, expectedUrl);
+
+
+        //Verify new page contains expected text ('Congratulations' or 'successfully logged in')
+        String expectedMessage = "Congratulations student. You successfully logged in!";
+        String pageSource = driver.getPageSource();
+        Assert.assertTrue(pageSource.contains(expectedMessage));
+
+        //Verify button Log out is displayed on the new page
+        WebElement logOutButton = driver.findElement(By.linkText("Log out"));
+        Assert.assertTrue(logOutButton.isDisplayed());
+
+        driver.quit();
+    }
 
     @Test
     public void incorrectUsernameTest(){
@@ -80,6 +123,7 @@ public class NegativeLoginTests {
         driver.quit();
 
     }
+
 
 
 }
