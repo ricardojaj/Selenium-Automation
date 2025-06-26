57b69a2eb28d0365ada7bb160c8941b88d41c85a
ricardojaj <rickanjos85@gmail.com>
2025-06-25 21:45:49 -0300

    Exceptions

---
+++ src/test/java/com/practicetestautomation/tests/exceptions/ExceptionsTests.java
@@ -0,0 +1,61 @@
+package com.practicetestautomation.tests.exceptions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.*;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ExceptionsTests {
+    private WebDriver driver;
+    private Logger logger;
+
+    @BeforeMethod(alwaysRun = true)
+    @Parameters("browser")
+    public void setUp(@Optional("chrome") String browser){
+        logger = Logger.getLogger(ExceptionsTests.class.getName());
+        logger.setLevel(Level.INFO);
+        logger.info("Running test in " + browser);
+        switch (browser.toLowerCase()){
+            case "chrome":
+                driver = new ChromeDriver();
+                break;
+            case "edge":
+                driver = new EdgeDriver();
+                break;
+            default:
+                driver = new ChromeDriver();
+                break;
+        }
+
+        driver.get("https://practicetestautomation.com/practice-test-exceptions/");
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown(){
+        driver.quit();
+        logger.info("Browser is closed");
+    }
+
+    @Test
+    public void testExceptionFunctionality(){
+        logger.info("Starting testLoginFunctionality");
+
+        WebElement addButton = driver.findElement(By.xpath("//button[@id=\"add_btn\"]"));
+        logger.info("Click Add button");
+        addButton.click();
+
+        //logger.info("Verify the confirmation message: " + expectedErrorMessage);
+        WebElement confirmationMessage = driver.findElement(By.id("confirmation"));
+        confirmationMessage.isDisplayed();
+
+
+    }
+
+
+
+}
